b0d4701ff4ab0f9afb954128184a4edbf8caa561
loordbarringtn <quantas@gmail.com>
2024-11-16 00:06:04 +0300

    fix ind

--- src/parser.js
+++ src/parser.js
@@ -3,18 +3,18 @@ import path from 'path';
 import yaml from 'js-yaml';
 
 const parseFile = (filepath) => {
-    const extname = path.extname(filepath).toLowerCase();
-    const fileContent = fs.readFileSync(filepath, 'utf-8');
+  const extname = path.extname(filepath).toLowerCase();
+  const fileContent = fs.readFileSync(filepath, 'utf-8');
 
-    switch (extname) {
-        case '.json':
-            return JSON.parse(fileContent);
-        case '.yaml':
-        case '.yml':
-            return yaml.load(fileContent);
-        default:
-            throw new Error(`Unsupported file format: ${extname}`);
-    }
+  switch (extname) {
+    case '.json':
+      return JSON.parse(fileContent);
+    case '.yaml':
+    case '.yml':
+      return yaml.load(fileContent);
+    default:
+      throw new Error(`Unsupported file format: ${extname}`);
+  }
 };
 
 export default parseFile;
