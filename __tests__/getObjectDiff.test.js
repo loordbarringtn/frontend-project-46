45336684b1ab72d8675057031469ff626839e974
loordbarringtn <quantas@gmail.com>
2024-12-06 23:57:43 +0300

    added tests

--- __tests__/getObjectDiff.test.js
+++ __tests__/getObjectDiff.test.js
@@ -9,7 +9,7 @@ import gendiff from '../src/index.js';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const getFileContent = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
-describe('gendiff formatters tests', () => {
+describe('gendiff function formatters tests', () => {
   test('стиль stylish с JSON файлами', () => {
     const file1Path = getFixturePath('file1.json');
     const file2Path = getFixturePath('file2.json');
@@ -29,5 +29,24 @@ describe('gendiff formatters tests', () => {
 
     const result = gendiff(file1Content, file2Content);
     expect(result).toBe(expectedResult);
+  });
+
+  test('стиль plain с JSON файлами', () => {
+    const file1Path = getFixturePath('file1.json');
+    const file2Path = getFixturePath('file2.json');
+    const expectedResult = getFileContent('expected_plain_output.txt'); // Читаем эталонный результат
+
+    expect(gendiff(file1Path, file2Path, 'plain')).toBe(expectedResult);
+  });
+
+  test('стиль plain с YAML файлами', () => {
+    const file1Path = getFixturePath('file1.yml');
+    const file2Path = getFixturePath('file2.yml');
+    const expectedResult = getFileContent('expected_plain_output.txt'); // Читаем эталонный результат
+
+    const file1Content = YAML.parse(file1Path); // Парсим YAML-файл 1
+    const file2Content = YAML.parse(file2Path); // Парсим YAML-файл 2
+
+    expect(gendiff(file1Content, file2Content, 'plain')).toBe(expectedResult);
   });
 });
